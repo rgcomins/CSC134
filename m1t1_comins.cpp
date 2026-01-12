c3f3b88a05fca74046c474c4101ffd065e0f5121
rgcomins <100152345+rgcomins@users.noreply.github.com>
2026-01-12 16:35:54 +0000

    Finished

---
+++ m1t1_comins.cpp
@@ -0,0 +1,13 @@
+//CSC 134
+// M1T1 (Module 1, Tutorial 1)
+// Hello, World!
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello CSC 134"<< endl; // ENDL
+    cout<< endl; //another new line
+    return 0;
+}
