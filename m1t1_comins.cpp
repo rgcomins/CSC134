db761c81bd6eb351b4815ec50ecdbdd9d2fe58e0
rgcomins <100152345+rgcomins@users.noreply.github.com>
2026-01-14 16:23:51 +0000

    Finished m1t1

--- m1t1_comins.cpp
+++ m1t1_comins.cpp
@@ -9,5 +9,15 @@ using namespace std;
 int main() {
     cout << "Hello CSC 134"<< endl; // ENDL
     cout<< endl; //another new line
+    cout << "Hello World!"<< endl; // ENDL
+    cout<< endl; //another new line<<
+    
+    string name;//new box to hold a name in
+    cout<< "What is your name? ";
+    cin >> name; //read in their answer
+    // finally, say hi to them
+    cout << "Well hi, " <<name<<"!" << endl;
+    
     return 0;
+
 }
