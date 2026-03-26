ebc6e68d6e5d8c86a6b736c57f0946bdf60b967a
rgcomins <100152345+rgcomins@users.noreply.github.com>
2026-03-26 15:22:43 +0000

    M4HW1 Done

--- m4/m4hw1.cpp
+++ m4/m4hw1.cpp
@@ -1,7 +1,7 @@
 /*CSC - 134
 M4HW1.cpp
 3/4/2026
-Comins
+Comins & Claude
 Assignment: Ask user for a number (1-12)
 then display the times table, as a table.
 */
@@ -10,19 +10,38 @@ then display the times table, as a table.
 using namespace std;
 
 int main() {
-   
-    
-    cout << "The Multiplication Table" << endl;
+
+    // Constants for the valid input range
     const int MIN = 1;
     const int MAX = 12;
-    int input = num_get >> input;
-    int num = MIN;
-    int multiplied;
-    cout << "Number\tNumber multiplied\n";
+
+    // Variables
+    int input;      // number entered by the user
+    int num;        // current row in the table
+    int multiplied; // result of num * input
+
+    // Prompt user for a number within the valid range
+    cout << "The Multiplication Table" << endl;
+    cout << "Enter a number (1-12): ";
+    cin >> input;
+
+    // Validate that input is within the accepted range
+    if (input < MIN || input > MAX) {
+        cout << "Invalid input. Please enter a number between 1 and 12." << endl;
+        return 1; // exit with error code
+    }
+
+    // Print table header
+    cout << "\nNumber\tResult" << endl;
     cout << "____________________________" << endl;
+
+    // Loop from MIN to MAX, printing each multiplication result
+    num = MIN;
     while (num <= MAX) {
         multiplied = num * input;
         cout << num << "\t" << multiplied << endl;
-        num++; //go to next number
+        num++; // advance to the next row
     }
+
+    return 0;
 }
