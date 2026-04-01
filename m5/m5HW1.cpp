6cd67cef71ad7a367dc588963400bb8406f1661b
rgcomins <100152345+rgcomins@users.noreply.github.com>
2026-04-01 15:50:59 +0000

    m5HW1 question 1

--- m5/m5HW1.cpp
+++ m5/m5HW1.cpp
@@ -26,7 +26,31 @@ int main() {
 
 // All full questions (function definitions) go here
 void question1() {
+    string month1,month2,month3;
+    double rainfall_month1,rainfall_month2,rainfall_month3;
+    double total_rainfall,average_rainfall;
+    
     cout << "Question 1" << endl;
+    cout <<  "Enter the name of month1: ";
+    cin >> month1;
+    cout << "Enter rainfaill for month1: ";
+    cin >> rainfall_month1;
+
+    cout <<  "Enter the name of month2: ";
+    cin >> month2;
+    cout << "Enter rainfaill for month2: ";
+    cin >> rainfall_month2;
+
+    cout <<  "Enter the name of month3: ";
+    cin >> month3;
+    cout << "Enter rainfaill for month3: ";
+    cin >> rainfall_month3;
+
+    total_rainfall = rainfall_month1 + rainfall_month2 + rainfall_month3;
+    average_rainfall = total_rainfall / 3;
+    cout << "The total rainfall was " << total_rainfall << endl;
+    cout << "the average rainfall was " << average_rainfall << endl;
+
 }
 
 void question2() {
