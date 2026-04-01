68aeafea67c6012ce39ca8250ad11529c58bc589
rgcomins <100152345+rgcomins@users.noreply.github.com>
2026-04-01 14:35:18 +0000

    Initial change for Homework

---
+++ m5/m5HW1.cpp
@@ -0,0 +1,34 @@
+// CSC 134
+// M5HW1 - Sample Setup
+// cominsr
+// 4/1/26
+// Not answering the questions, just setting up the structure.
+#include <iostream>
+using namespace std;
+
+// List all question prototypes here
+void question1();
+void question2();
+// rest go here
+
+
+// main() here
+int main() {
+
+    // Question 6: Print a menu so they can choose a question
+    // Until I do question 6, they will all run one at a time
+
+    question1();
+    question2();
+    // and so on
+    return 0;
+}
+
+// All full questions (function definitions) go here
+void question1() {
+    cout << "Question 1" << endl;
+}
+
+void question2() {
+    cout << "Question 2" << endl;
+}
