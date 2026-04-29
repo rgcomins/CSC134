cbafe61ae9b8cf7d57d68ffcca7b2e62ed70c6d8
rgcomins <100152345+rgcomins@users.noreply.github.com>
2026-04-29 15:33:29 +0000

    m7t2 first attempt

---
+++ m7/m7t2.cpp
@@ -0,0 +1,35 @@
+/*
+M72 - Rectangles
+with some pointer examples
+cominsr
+4/29/26
+*/
+
+#include <iostream>
+using namespace std;
+
+void setName(string &name);
+
+int main() {
+
+    string name = "Bob";
+    string * pName = &name;
+
+    setName (name);
+    
+    cout << "name = " <<name << endl;
+    cout << "pName =  " << pName << endl;
+    cout << "*pName = " << *pName << endl;
+
+    return 0;
+
+    
+}
+
+void setName(string &name) {
+
+    //uses pass-by-reference, changing the original
+    cout << "What's your name? ";
+    cin >> name;
+
+}
